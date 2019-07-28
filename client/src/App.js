1e00eca69f271220087c560d8970af1933a89953
am-gforce <47460424+am-gforcehub@users.noreply.github.com>
2019-07-27 22:10:55 -0500

    added code

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <div className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h2>Welcome to React</h2>
+//         </div>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/home";
+import Bookshelf from "./pages/bookshelf";
+import Search from "./pages/search";
+import NavBar from "./components/NavBar/NavBar";
+
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Search} />
+      <Route exact path="/bookshelf" component={Bookshelf} />
+      <Route exact path="/search" component={Search} />
+    </div>
+  </Router>
+);
 
 export default App;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,38 @@
-import axios from "axios";
+// import axios from "axios";
 
-// The getRecipes method retrieves recipes from the server
-// It accepts a "query" or term to search the recipe api for
+// // The getRecipes method retrieves recipes from the server
+// // It accepts a "query" or term to search the recipe api for
+// export default {
+//   getRecipes: function(query) {
+//     return axios.get("/api/recipes", { params: { q: query } });
+//   }
+// };
+import axios from "axios";
+import APIKEY from "../config/keys";
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+// Export an object containing methods use for accessing googlebooks API
 export default {
-  getRecipes: function(query) {
-    return axios.get("/api/recipes", { params: { q: query } });
+  // Search for books
+  searchBooks: query => {
+    //console.log(query);
+    return axios.get(URL + query + APIKEY);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    console.log(bookData);
+    return axios.post("/api/books", bookData);
+  },
+  // Gets all saved books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
   }
 };
